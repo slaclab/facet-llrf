24246950b94c7d1a480a7a8ffc18b26d65cd59f2
Mitch D'Ewart <mdewart@slac.stanford.edu>
2020-06-16 21:13:05 -0700

    Remove PCAV code

--- firmware/common/Llrf/ruckus.tcl
+++ firmware/common/Llrf/ruckus.tcl
@@ -3,9 +3,6 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 loadSource -dir  "$::DIR_PATH/rtl/"
-#loadSource -path  "$::DIR_PATH/netlist/example_stub.vhd"
-#loadSource -path  "$::DIR_PATH/netlist/example.dcp"
-loadSource -path  "$::DIR_PATH/netlist/AppModel.vhd"
 
 loadIpCore -path "$::DIR_PATH/coregen/ila_0.xci"
 
@@ -20,6 +17,3 @@ set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
 
 set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY rebuilt [get_runs synth_1]
 set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING on [get_runs synth_1]
-
-# Force synth_1 to be stale between runs incase the sysgen .DCP file changes ( work around for a bug in Vivado)
-#exec touch [get_files {MrDspCoreWrapper.vhd}]
