40a0017f7e51124fe861d5c3cfdeba2ffc0be5f6
kuktaekim <ktkim@slac.stanford.edu>
2023-05-09 11:05:01 -0700

    Made new targets for the test

---
+++ firmware/targets/Llrf/hdl/AppOpts.vhd
@@ -0,0 +1,22 @@
+-------------------------------------------------------------------------------
+-- This file is part of 'LCLS2 AMC Carrier Firmware'.
+-- It is subject to the license terms in the LICENSE.txt file found in the
+-- top-level directory of this distribution and at:
+--    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+-- No part of 'LCLS2 AMC Carrier Firmware', including this file,
+-- may be copied, modified, propagated, or distributed except according to
+-- the terms contained in the LICENSE.txt file.
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library surf;
+use surf.StdRtlPkg.all;
+
+package AppOpts is
+
+  constant APP_TIMING_MODE_C : integer := 1; -- 1 or 2 (LCLS-I/II)
+  constant UPCONVERT_V2_C    : boolean := false;
+
+end AppOpts;
